ac7f659cf75afe7406893322c361e7c9b9947e9d
SheikhAlUsama <82088182+SheikhAlUsama@users.noreply.github.com>
2021-07-01 01:21:36 +0500

    Add files via upload
    assignment 13 objects done

---
+++ app.js
@@ -0,0 +1,51 @@
+// 1. Suppose You have an array of object
+// var itemsArray = [
+// {name:”juice”,price:”50”, quantity:”3”},
+// {name:”cookie”,price:”30”, quantity:”9”},
+// {name:”shirt”,price:”880”, quantity:”1”},
+// {name:”pen”,price:”100”, quantity:”2”}];
+// Calculate total price of each item and all items;
+
+// var itemsArray = [
+    // {name:"juice",price:50, quantity:"3"},
+    // {name:"cooki",price:30, quantity:"9"},
+    // {name:"shirt",price:880, quantity:"1"},
+    // {name:"pen",price:100, quantity:"2"}];
+    
+    // console.log(itemsArray[0].price*itemsArray[0].quantity);
+    // console.log(itemsArray[1].price*itemsArray[1].quantity);
+    // console.log(itemsArray[2].price*itemsArray[2].quantity);
+    // console.log(itemsArray[3].price*itemsArray[3].quantity);
+
+    // console.log(itemsArray[0].price+itemsArray[1].price+itemsArray[2].price+itemsArray[3].price)
+
+        // 2. Create an object with properties name, email, password, age,
+        // gender, city, country.
+        // Check if age and country properties exist in object or not.
+        // Also check firstName and lastName properties in object.
+
+        // var identity = {
+        //     name : {
+        //         firstname : "Muhammad",
+        //         lastName : "Usama",
+        //     },
+        //     email : "usama@gmial.com" ,
+        //     password : "*****",
+        //     age : "21" ,
+        //     gender : "male",
+        //     city : "karachi",
+        //     country :"pakistan"
+        // }
+        // console.log(identity.age,identity.country)
+        // console.log(identity.name.firstname,identity.name.lastName);
+        
+
+//         3. Create a constructor function with some properties. Now
+// create multiple records using the constructor.
+//  var plan = function (name, price, space, transfer, pages) {
+//      this.name = name;
+//      this.price = price;
+//      this.space = space;
+//      this.transfer = transfer;
+//      this.pages = pages;
+//      }
